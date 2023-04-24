4cdc3a446f497f85cda039111297049899a38233
Aesop <ee.changsub@gmail.com>
2023-04-24 21:18:35 +0900

    config validation 추가

--- src/config/validate-schema.ts
+++ src/config/validate-schema.ts
@@ -3,6 +3,6 @@ import * as Joi from 'joi';
 export function validateSchema() {
   return Joi.object({
     NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
-    port: Joi.number(),
+    server_port: Joi.number(),
   });
 }
