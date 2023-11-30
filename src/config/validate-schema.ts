d379f0dcce00b3a822afaa40c07c19854f553223
Aesop <aesop@coinone.com>
2023-11-30 14:16:14 +0900

    Add local to NODE_ENV

--- src/config/validate-schema.ts
+++ src/config/validate-schema.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export function validateSchema() {
   return Joi.object({
-    NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
+    NODE_ENV: Joi.string().valid('local', 'dev', 'prod', 'test').default('dev'),
     PORT: Joi.number().required(),
     NOTION_API_KEY: Joi.string().required(),
     TASK_TABLE_ID: Joi.string().required(),
